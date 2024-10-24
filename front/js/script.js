8488153a15154db7009b4271c3a0319afa908411
Gulcan COSKUN <gulcancoskun.a@gmail.com>
2024-10-24 11:33:41 +0200

    change the file name as script.js

---
+++ front/js/script.js
@@ -0,0 +1,10 @@
+// DISPLAY ALL THE PRODUCTS ON THE HOME PAGE
+// The name of the file must be script.js because in the index.html, at the bottom, the linked script folder is script.js
+// 1. Use fetch method, it is a simple GET request, FETCH() is returning a promise. 
+// 2. Use then() which resolves the promise to a response
+
+function fetchData() {
+    console.log("START FETCH");
+}
+
+fetchData();
