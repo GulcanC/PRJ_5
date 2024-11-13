53caf02163860e40dfa819180d8eefb1cbf85309
Gulcan COSKUN <gulcancoskun.a@gmail.com>
2024-11-13 14:23:52 +0100

    put alert messages

--- front/js/product.js
+++ front/js/product.js
@@ -38,6 +38,7 @@ function getProduct() {
         console.log(myProduct);
         if (myProduct) {
             displayProduct(myProduct);
+            addToCart(myProduct);
         }
 
         // Then catch the errors
@@ -77,7 +78,7 @@ function displayProduct(myProduct) {
 
     // get colors option
     // here colors is a specific name which comes from Product.js, it is an array.
-  
+
     myProduct.colors.forEach(oneColor => {
         console.log(oneColor)
         let option = document.createElement('option');
@@ -86,6 +87,141 @@ function displayProduct(myProduct) {
         option.innerHTML = oneColor;
 
     });
+
+
+}
+
+// add selected product to cart
+
+// call DOM elements
+
+let addButton = document.getElementById('addToCart');
+let quantity = document.getElementById('quantity');
+let color = document.getElementById('colors');
+
+
+let prdLocalStorageStr = localStorage.getItem('selectedPrd');
+console.log(prdLocalStorageStr); // at first it is null
+
+let prdLocalStorage = JSON.parse(prdLocalStorageStr)
+console.log(prdLocalStorage);  // at first it is null
+
+
+function addToCart(myProduct) {
+    addButton.addEventListener('click', (event) => {
+        // create object 
+        let productObj = {
+            productId: productId,
+            productColor: color.value,
+            productQty: Number(quantity.value),
+        }
+
+        // add another alert messages
+
+        if ((quantity.value == 0 || quantity.value == null) && (color.value == 0 || color.value == null)) {
+            alert('⚠️ Please choose a color and quantity ! ')
+            window.location.reload();
+        }
+
+        else if ((quantity.value == 0 || quantity.value == null)) {
+            alert('⚠️ Please choose a quantity ! ')
+            window.location.reload();
+        }
+
+        else if ((color.value == 0 || color.value == null)) {
+            alert('⚠️ Please choose a color ! ')
+            window.location.reload();
+        }
+
+
+        // if twe chose an item between 1 and 100, push it to local storage
+        else if (quantity.value > 0 && quantity.value <= 100) {
+
+
+            // create a general alert message
+            function alertMessage() {
+
+                alert(
+
+                    `💜💜💜 The selected product was added to the cart!
+
+                    ✔️ Product name: ${myProduct.name}
+                    ✔️ Product color: ${productObj.productColor}
+                    ✔️ Selected product quantity: ${productObj.productQty}
+                    ✔️ Total quantity for the same product: ${newQty}
+                    `
+                );
+            }
+
+
+            if ((prdLocalStorage == null || prdLocalStorage == 0)) {
+                prdLocalStorage = [];
+                prdLocalStorage.push(productObj);
+                localStorage.setItem('selectedPrd', JSON.stringify(prdLocalStorage));
+
+                // call alert message here
+                alertMessage();
+                // go to the cart page
+                location.assign('cart.html')
+
+            }
+
+            // if the same item exist in the local storage, increase the number of the item. 
+
+            else {
+                var filterProduct = prdLocalStorage.findIndex(
+                    item => item.productId === productId && item.productColor === color.value
+                );
+                console.log(filterProduct);
+
+                if (filterProduct >= 0) {
+
+                    var newQty = Number(productObj.productQty) + Number(prdLocalStorage[filterProduct].productQty);
+                    console.log(newQty);
+                    prdLocalStorage[filterProduct].productQty = newQty;
+                    localStorage.setItem("selectedPrd", JSON.stringify(prdLocalStorage));
+
+                  
+                    // call alert message here
+                    alertMessage();
+                    // go to the cart page
+                    location.assign('cart.html')
+                }
+
+                else {
+                    prdLocalStorage.push(productObj);
+                    localStorage.setItem('selectedPrd', JSON.stringify(prdLocalStorage));
+                    // call alert message here
+                    alertMessage();
+                    // go to the cart page
+                    location.assign('cart.html')
+                }
+            }
+
+
+
+        }
+
+        else if (quantity.value > 100) {
+            alert('⚠️ Please choose a quantity smaller or equal to 100 ! ')
+            window.location.reload();
+        }
+        else if (quantity.value < 0) {
+            alert('⚠️ You can not choose negatif value ! ')
+            window.location.reload();
+        }
+
+
+    })
+
+
+
+
+    // we will put the data on the local storage
+    // we will use the localStorage object, and we will call setItem method
+    // setItem method takes two arguments, first is the name of the entry, second is data
+    // with stringfy() method, we will convert the data to the string because we can store only string data types in the local storage
+    // next I will store an empty array to the local storage, 
 
 }
 
