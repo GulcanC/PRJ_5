c2851885add85c1986d65aa15c1677feca6ce294
Gulcan COSKUN <gulcancoskun.a@gmail.com>
2024-11-14 09:52:30 +0100

    complete product.js and start for the cart.js

---
+++ front/js/cart.js
@@ -0,0 +1,10 @@
+
+// here the important thing is the key and value name in the local storage. 
+// When I send the selected items to local storage, I gave the name for the key "selectedPrd", it is importrant. 
+// AQnd this selected product has a value which contains, productId, productColor, productQty, these names are also important. I can not change them in the cart.js
+// they come from the product.js => 
+ /*   let productObj = {
+        productId: productId,
+        productColor: color.value,
+        productQty: Number(quantity.value),
+    } */
